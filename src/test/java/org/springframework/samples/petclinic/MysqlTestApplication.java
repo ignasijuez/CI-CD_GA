53d1837925e4dc9c1fa89e33e2c1831b8bb8db64
ignasijuez <ignasi.juezguirao@student.kuleuven.be>
2024-10-16 12:33:38 +0200

    a

--- src/test/java/org/springframework/samples/petclinic/MysqlTestApplication.java
+++ src/test/java/org/springframework/samples/petclinic/MysqlTestApplication.java
@@ -16,6 +16,7 @@
 
 package org.springframework.samples.petclinic;
 
+import org.junit.jupiter.api.Disabled;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
